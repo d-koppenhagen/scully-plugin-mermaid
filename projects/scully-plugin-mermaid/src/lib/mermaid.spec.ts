071680c82a20b5a7a244d103806cd9f42a596555
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-09-21 07:29:20 +0200

    test: provide first simple test

--- projects/scully-plugin-mermaid/src/lib/mermaid.spec.ts
+++ projects/scully-plugin-mermaid/src/lib/mermaid.spec.ts
@@ -1,49 +1,49 @@
 import { HandledRoute, setPluginConfig } from '@scullyio/scully';
-import MermaidAPI from 'mermaid/mermaidAPI';
 
 import { getMermaidPlugin } from './index';
-import { mermaidPlugin } from './mermaid';
+import { MermaidPluginConfig } from './interfaces';
+import { mermaidPlugin, getSvg } from './mermaid';
 
 describe('mermaidPlugin', () => {
-  let defaultValidHtml: string;
+  let defaultHtml: string;
+  let defaultMermaidChart: string;
+  let mermaidConfig: MermaidPluginConfig;
   let defaultRouteDataConfig: HandledRoute;
-  let mermaidConfig: MermaidAPI.Config;
   let MermaidPlugin: any;
 
   beforeEach(() => {
-    defaultValidHtml = `<body>
-  <h1>Before Blog Main Content</h1>
-  <div class="blog">
-    <p>Some content before headings</p>
-    <div id="mermaid"></div>
-    <h1 id="h1-1">H1-1</h1>
-    <p>foo</p>
-    <h2 id="h2-1">H2-1</h2>
-    <span>bar</span>
-    <h3 id="h3-1">H3-1</h3>
-    baz
-    <h4 id="h4-1">H4-1</h4>
-    <h5 id="h5-1">H5-1</h5>
-    <h6 id="h6-1">H6-1</h6>
-    <h4 id="h4-2">H4-2</h4>
-    <h1 id="h1-2">H1-2</h1>
-    <h2 id="h2-2">H2-2</h2>
-  </div>
-</body>`;
-
+    defaultHtml = `<html><head></head><body>PLACEHOLDER</body></html>`;
+    defaultMermaidChart = `
+pie
+  "Dogs" : 386
+  "Cats" : 85
+  "Rats" : 15
+`;
     defaultRouteDataConfig = {
-      type: '',
-      route: '/foo/bar',
-      data: {
-        language: 'de',
-      },
+      route: 'foo',
     };
-
     mermaidConfig = {};
     MermaidPlugin = getMermaidPlugin();
+
+    const getSvgMock = jest.fn(getSvg);
+    getSvgMock.mockReturnValue(Promise.resolve('<svg></svg>'));
   });
-  it('should return the HTML including mermaid by respecting the configured options', async () => {
-    const html = await mermaidPlugin(defaultValidHtml, defaultRouteDataConfig);
-    expect(html).toEqual('');
+
+  afterEach(() => {
+    MermaidPlugin = undefined;
+  });
+
+  it('should not render anything when default selector does not match', async () => {
+    const testHtml = defaultHtml.replace(
+      'PLACEHOLDER',
+      `<pre><code>${defaultMermaidChart}</code></pre>`,
+    );
+    const html = await mermaidPlugin(testHtml, defaultRouteDataConfig);
+    expect(html).toEqual(`<html><head></head><body><pre><code>
+pie
+  "Dogs" : 386
+  "Cats" : 85
+  "Rats" : 15
+</code></pre></body></html>`);
   });
 });
