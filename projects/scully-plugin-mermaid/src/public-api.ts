fed8729e2dddc8abfb89af0fea35b623f0935e9d
Danny Koppenhagen <mail@d-koppenhagen.de>
2020-09-21 07:27:20 +0200

    fix: export MermaidPluginConfig

--- projects/scully-plugin-mermaid/src/public-api.ts
+++ projects/scully-plugin-mermaid/src/public-api.ts
@@ -2,4 +2,5 @@
  * Public API Surface of scully-plugin-mermaid
  */
 export { getMermaidPlugin } from './lib/index';
+export { MermaidPluginConfig } from './lib/interfaces';
 export { MermaidPluginName } from './lib/constants';
